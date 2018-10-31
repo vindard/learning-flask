78bceed81313892a3c4dd0b367f62fb7bbfc37f2
vindard <vinda.rd@gmail.com>
2018-10-30 22:53:27 -0400

    (bugfix): include skipped module

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
 
