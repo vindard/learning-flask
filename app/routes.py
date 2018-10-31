b31f5c86c0bfe68b2722d52667a56186a08fb837
vindard <vinda.rd@gmail.com>
2018-10-30 22:52:30 -0400

    (bugfix): deleted in prior commit accidentally

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
-from app.forms import 
+from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user
 from app.models import User
 from werkzeug.urls import url_parse
