a6da487e03844453c5eb381c24258180e2634c5a
vindard <pgp@arvinda.me>
2019-02-26 11:27:22 -0400

    move html from view function to...
    the new templates folder using `render_template` Flask method

--- myproj/routes.py
+++ myproj/routes.py
@@ -1,16 +1,8 @@
+from flask import render_template
 from myproj import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
-    return '''
-    <html>
-        <head>
-            <title>Home Page - Microblog</title>
-        </head>
-        <body>
-            <h1>Hello, ''' + user['username'] + '''!</h1>
-        </body>
-    </html>
-    '''
+    return render_template('index.html', title='Home', user=user)
